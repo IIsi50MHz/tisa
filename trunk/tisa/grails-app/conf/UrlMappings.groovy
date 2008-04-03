b62c94a2df4b60ca95477e186886a06befd43214
michal.hantl <michal.hantl@2c020212-9f48-0410-af96-1900c6b14a55>
2008-04-03 21:06:03 +0000

    kuciii

--- trunk/tisa/grails-app/conf/UrlMappings.groovy
+++ trunk/tisa/grails-app/conf/UrlMappings.groovy
@@ -4,12 +4,13 @@ class UrlMappings {
 			"/akce/$action?/$id?" (controller:"front_akce")
 			"/mista/$action?/$id?" (controller:"front_misto")
 			"/rezervace/$action?/$id?" (controller:"front_rezervace")
+			"/spravce/$action?/$id?" (controller:"front_spravce")
 				
 
 			"/admin/" (controller:"akce")
 			"/admin/akce/$action?/$id?" (controller:"akce")
 			"/admin/misto/$action?/$id?" (controller:"misto")
-        		"/admin/mesto/$action?/$id?" (controller:"mesto")
+      "/admin/mesto/$action?/$id?" (controller:"mesto")
 			"/admin/rezervace/$action?/$id?" (controller:"rezervace")
 			"/admin/rozmisteni/$action?/$id?" (controller:"rozmisteni")
 			"/admin/vyprseni/$action?/$id?" (controller:"zpusob_vyprseni_rezervace")
---
+++ trunk/tisa/grails-app/controllers/Front_SpravceController.groovy
@@ -0,0 +1,21 @@
+class Front_spravceController {
+
+    def register = {
+		}
+
+    def login = {
+		}
+		
+		def save ={
+				params.vsechna_prava = 0
+		    def spravce = new Spravce(params)
+        if(!spravce.hasErrors() && spravce.save()) {
+            flash.message = "Spravce ${spravce.id} created"
+            redirect(action:login)
+        }
+        else {
+            render(view:'register',model:[spravce:spravce])
+        }
+    }
+
+}
