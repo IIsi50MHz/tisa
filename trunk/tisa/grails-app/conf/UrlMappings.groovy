6590e6087e2c36e9ce9bb34b6aa5a0b75a70b50d
michal.hantl <michal.hantl@2c020212-9f48-0410-af96-1900c6b14a55>
2008-04-05 11:38:11 +0000

    frontend fix

--- trunk/tisa/grails-app/conf/UrlMappings.groovy
+++ trunk/tisa/grails-app/conf/UrlMappings.groovy
@@ -1,19 +1,19 @@
 class UrlMappings {
     static mappings = {
 			"/" (controller:"front_end", view:"index")
-			"/akce/$action?/$id?" (controller:"front_akce")
-			"/mista/$action?/$id?" (controller:"front_misto")
-			"/rezervace/$action?/$id?" (controller:"front_rezervace")
-			"/spravce/$action?/$id?" (controller:"front_spravce")
+			"/akce/$action?/$id?" (controller:"frontAkce")
+			"/mista/$action?/$id?" (controller:"frontMisto")
+			"/rezervace/$action?/$id?" (controller:"frontRezervace")
+			"/spravce/$action?/$id?" (controller:"frontSpravce")
 				
 
-			"/admin/" (controller:"akce")
+			"/admin/" (controller:"admin")
 			"/admin/akce/$action?/$id?" (controller:"akce")
 			"/admin/misto/$action?/$id?" (controller:"misto")
       "/admin/mesto/$action?/$id?" (controller:"mesto")
 			"/admin/rezervace/$action?/$id?" (controller:"rezervace")
 			"/admin/rozmisteni/$action?/$id?" (controller:"rozmisteni")
-			"/admin/vyprseni/$action?/$id?" (controller:"zpusob_vyprseni_rezervace")
+			"/admin/vyprseni/$action?/$id?" (controller:"zpusobVyprseniRezervace")
 			"/admin/spravce/$action?/$id?" (controller:"spravce")
 
       "500"(view:'/error')
