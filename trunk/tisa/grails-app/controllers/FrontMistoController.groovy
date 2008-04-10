1c8973a5a8828a4e1b7ec9d024d1e656b4642480
tomas.pytlik <tomas.pytlik@2c020212-9f48-0410-af96-1900c6b14a55>
2008-04-10 15:10:15 +0000

    revize front kontroleru (umazani nepoterebnych actions), prodlouzeni vypisu ve frontendu (z 10 polozek na 20), uprava zobrazeni ve frontendu (nekde byl a nekde nebyl odkaz ,,detaily akce,,, tak ted uz je vsude :))

--- trunk/tisa/grails-app/controllers/FrontMistoController.groovy
+++ trunk/tisa/grails-app/controllers/FrontMistoController.groovy
@@ -9,7 +9,7 @@ class FrontMistoController {
     def where = "Misto"
     
     def list = {
-        if(!params.max) params.max = 10
+        if(!params.max) params.max = 20
         [ mistoList: Misto.list( params ) ]
     }
 
@@ -22,65 +22,5 @@ class FrontMistoController {
         }
         else { return [ misto : misto ] }
     }
-
-    def delete = {
-        def misto = Misto.get( params.id )
-        if(misto) {
-            misto.delete()
-            flash.message = message(code:"tisa.controllers.deleted", args:["${where}", "${params.id}"]) 
-            redirect(action:list)
-        }
-        else {
-            flash.message = message(code:"tisa.controllers.notfound", args:["${where}", "${params.id}"]) 
-            redirect(action:list)
-        }
-    }
-
-    def edit = {
-        def misto = Misto.get( params.id )
-
-        if(!misto) {
-            flash.message = message(code:"tisa.controllers.notfound", args:["${where}", "${params.id}"]) 
-            redirect(action:list)
-        }
-        else {
-            return [ misto : misto ]
-        }
-    }
-
-    def update = {
-        def misto = Misto.get( params.id )
-        if(misto) {
-            misto.properties = params
-            if(!misto.hasErrors() && misto.save()) {
-                flash.message = message(code:"tisa.controllers.updated", args:["${where}", "${params.id}"]) 
-                redirect(action:show,id:misto.id)
-            }
-            else {
-                render(view:'edit',model:[misto:misto])
-            }
-        }
-        else {
-            flash.message = message(code:"tisa.controllers.notfound", args:["${where}", "${params.id}"]) 
-            redirect(action:edit,id:params.id)
-        }
-    }
-
-    def create = {
-        def misto = new Misto()
-        misto.properties = params
-        return ['misto':misto]
-    }
-
-    def save = {
-        def misto = new Misto(params)
-        if(!misto.hasErrors() && misto.save()) {
-            flash.message = message(code:"tisa.controllers.created", args:["${where}", "${params.id}"]) 
-            redirect(action:show,id:misto.id)
-        }
-        else {
-            render(view:'create',model:[misto:misto])
-        }
-    }
-
+    
 }
