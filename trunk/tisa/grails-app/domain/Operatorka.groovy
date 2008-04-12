ac9aea996e33c74fe1dc2ce75de2badcb0342102
michal.hantl <michal.hantl@2c020212-9f48-0410-af96-1900c6b14a55>
2008-04-12 12:58:47 +0000

    operatorka fix?

--- trunk/tisa/grails-app/domain/Operatorka.groovy
+++ trunk/tisa/grails-app/domain/Operatorka.groovy
@@ -2,26 +2,27 @@ import java.security.MessageDigest;
 
 class Operatorka extends UserStub {
 	
-          def belongsTo = Misto
-          Misto misto   
-          Boolean vsechny_mista = false
-    
-           //TODO: nefacha 
-           def listAkce() {
-                  def list = []  
-                    if (vsechny_mista){
-                          list = Akce.list() 
-                    } else {
-                          def i = 0
-                         
-                            for(rozmisteni in misto.rozmisteni){
-                              for(akce in rozmisteni.akce) {
-                                list[i++] = akce;
-                              }
-                            }
-                          
-                    }
-             return list
-           }
-                      
+			def belongsTo = Misto
+			Misto misto   
+			Boolean vsechny_mista = false
+
+			static constraints = {
+					misto()
+					vsechny_mista()
+      }
+		
+			 //TODO: nefacha 
+			 def listAkce() {
+							def list = []  
+								if (vsechny_mista){
+											list = Akce.list() 
+								} else {
+											def i = 0
+											for(akce in misto.rozmisteni.akce) {
+												list[i++] = akce;
+											}
+								}
+				 return list
+			 }
+
 }
